50326883965d53cff3e421417029d85f1f6c3010
igor <iegurtov7@gmai.com>
2025-11-14 03:32:12 +0300

    test

---
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by lulukun on 2025/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
