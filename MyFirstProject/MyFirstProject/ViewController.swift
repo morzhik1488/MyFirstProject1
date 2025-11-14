da1e4c8c57b89c09f31a266757838b00622a4b74
igor <iegurtov7@gmai.com>
2025-11-14 04:48:02 +0300

    sdelal

--- MyFirstProject/MyFirstProject/ViewController.swift
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var p1: UILabel!
+    @IBOutlet weak var p2: UILabel!
+    @IBOutlet weak var p3: UILabel!
+    @IBOutlet weak var p4: UILabel!
+    @IBOutlet weak var p5: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        p1.text = "1.Карри"
+        p2.text = "2.Ципленок"
+        p3.text = "3.Гавайская"
+        p4.text = "4.Веган"
+        p5.text = "5.Колбасня"
     }
 
 
